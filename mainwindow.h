271c02e1ac5340ce2372d54477e22770c1dd2b07
Artem <mail@artempal.ru>
2016-11-28 18:35:44 +0300

    объявление переменных файлов, слотов кнопок и меню, подключение стандартных библиотек

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QDebug>
+#include <QFile>
+#include <QByteArray>
+#include <QString>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +19,18 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void get_book(); //функция чтения из книги
 
 private:
+    QString file_name; //имя открытого файла
+    QString file_dir; //имя папки
+    QString book_text; //текст открытой книги
     Ui::MainWindow *ui;
+private slots:
+    void dir_open(); //функция открытия папки
+    void book_open(); //функция открытия книги
+    void prev_page(); //функции кнопок
+    void next_page();
 };
 
 #endif // MAINWINDOW_H
